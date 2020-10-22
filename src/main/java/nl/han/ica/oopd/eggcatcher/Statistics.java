5b62875ca35af6fcad9aca12ccbe92b50e8e7031
Niels Bosman <niels.bosman@student.graafschapcollege.nl>
2020-10-22 20:51:09 +0200

    Cleanup

--- src/main/java/nl/han/ica/oopd/eggcatcher/Statistics.java
+++ src/main/java/nl/han/ica/oopd/eggcatcher/Statistics.java
@@ -3,16 +3,17 @@ package nl.han.ica.oopd.eggcatcher;
 import nl.han.ica.oopg.persistence.FilePersistence;
 
 public class Statistics {
-    private static FilePersistence persistence;
+    private static       FilePersistence persistence;
+    private static final String          path = "main/java/nl/han/ica/oopd/eggcatcher/media/highscore.txt";
 
     public static void setHighscore(int highscore) {
-        persistence = new FilePersistence("main/java/nl/han/ica/oopd/eggcatcher/media/highscore.txt");
+        persistence = new FilePersistence(path);
 
-        if(persistence.fileExists()) persistence.saveData(String.valueOf(highscore));
+        if (persistence.fileExists()) persistence.saveData(String.valueOf(highscore));
     }
 
     public static int getHighscore() {
-        persistence = new FilePersistence("main/java/nl/han/ica/oopd/eggcatcher/media/highscore.txt");
+        persistence = new FilePersistence(path);
 
         return persistence.fileExists() ? Integer.parseInt(persistence.loadDataString()) : 0;
     }
